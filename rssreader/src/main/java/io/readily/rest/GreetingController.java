d422dc7972874a20f1e7b303b5cf8a785fc5d985
asingh7 <asingh7@ECCICSA6836.uspto.gov>
2018-11-15 13:25:55 -0500

    sample spring boot app

---
+++ rssreader/src/main/java/io/readily/rest/GreetingController.java
@@ -0,0 +1,20 @@
+package io.readily.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @Value("${name}")
+    private String name;
+    
+    @Value("${message}")
+    private String message;
+    
+    @RequestMapping("/greeting")
+    public String greeting() {
+        return message + "," + name;
+    }
+}
