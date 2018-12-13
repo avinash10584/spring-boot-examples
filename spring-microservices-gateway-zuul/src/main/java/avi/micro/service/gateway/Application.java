54c1fc17679ef23f1901c6b99a358db787f31469
avinash10584 <avinash@outlook.com>
2018-12-13 16:40:18 -0500

    Added ribbon load balancing for service

--- spring-microservices-gateway-zuul/src/main/java/avi/micro/service/gateway/Application.java
+++ spring-microservices-gateway-zuul/src/main/java/avi/micro/service/gateway/Application.java
@@ -3,9 +3,11 @@ package avi.micro.service.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableZuulProxy
+@RestController
 public class Application {
 
     public static void main(String[] args) {
