c8466bb9aecfafaa6803590c1f1323d1c20e241f
asingh7 <asingh7@ECCICSA6836.uspto.gov>
2019-02-11 15:59:09 -0500

    Updates to config

--- spring-microservices-rssfeed-api-reactive/src/main/java/avi/micro/service/rssreader/MongoConfig.java
+++ spring-microservices-rssfeed-api-reactive/src/main/java/avi/micro/service/rssreader/MongoConfig.java
@@ -3,6 +3,7 @@ package avi.micro.service.rssreader;
 import java.io.IOException;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import com.mongodb.MongoClient;
@@ -10,6 +11,7 @@ import com.mongodb.MongoClient;
 import cz.jirutka.spring.embedmongo.EmbeddedMongoFactoryBean;
 import de.flapdoodle.embed.mongo.distribution.Version;
 
+@Configuration
 public class MongoConfig {
     
     private static final String MONGO_DB_URL = "localhost";
