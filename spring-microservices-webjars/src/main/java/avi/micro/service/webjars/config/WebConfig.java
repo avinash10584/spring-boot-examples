e36e7864f79461cbcebd4476103c90955a135359
avina <avina@DESKTOP-4GS4D5B.fios-router.home>
2019-02-24 20:16:58 -0500

    Trigger webhook

--- spring-microservices-webjars/src/main/java/avi/micro/service/webjars/config/WebConfig.java
+++ spring-microservices-webjars/src/main/java/avi/micro/service/webjars/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    	
       registry.addResourceHandler("/lib/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
